8bf62540c9ad3628cb2d9d701a4b15482baa1402
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-05-03 12:32:09 -0400

    new api

--- js/app.js
+++ js/app.js
@@ -2,18 +2,17 @@
 const form = document.querySelector('#songForm');
 const resultsDiv = document.querySelector('#results');
 
-//get track
-async function getTrackId(songName, token) {
-  const searchResponse = await fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(songName)}&type=track&limit=1`, {
+async function getArtistId(artistName, token) {
+  const searchResponse = await fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(artistName)}&type=artist&limit=1`, {
     headers: {
       'Authorization': 'Bearer ' + token
     }
   });
   const searchData = await searchResponse.json();
-  return searchData.tracks.items[0].id;
+  return searchData.artists.items[0].id;
 }
 
-async function getRecommendations(songName) {
+async function getRecommendations(artistName) {
   try {
     // Request access token
     const response = await fetch('https://accounts.spotify.com/api/token', {
@@ -27,32 +26,36 @@ async function getRecommendations(songName) {
     const data = await response.json();
     const token = data.access_token;
 
-    // Get track ID for the song
-    const trackId = await getTrackId(songName, token);
+    // Get artist ID for the artist
+    const artistId = await getArtistId(artistName, token);
 
-    // Request recommendations based on track ID
-    const recommendationsResponse = await fetch(`https://api.spotify.com/v1/recommendations?seed_tracks=${trackId}`, {
+    // Request recommendations based on artist ID
+    const recommendationsResponse = await fetch(`https://api.spotify.com/v1/recommendations?seed_artists=${artistId}`, {
       headers: {
         'Authorization': 'Bearer ' + token
       }
     });
     const recommendationsData = await recommendationsResponse.json();
 
-    // Display recommended artists
+    // Display similar artists
     let artists = recommendationsData.tracks.map(track => track.artists[0].name);
-    resultsDiv.innerHTML = `<p>Recommended artists based on ${songName}: ${artists.join(', ')}</p>`;
+    resultsDiv.innerHTML = `<p>Similar artists to ${artistName}: ${artists.join(', ')}</p>`;
   } catch (error) {
     console.error(error);
   }
 }
 
-
 // Function to handle form submission
 function handleFormSubmit(event) {
-    event.preventDefault();
-    const songName = document.querySelector('#song').value;
-    getRecommendations(songName);
-    }
+  event.preventDefault();
+  const artistName = document.querySelector('#artist').value;
+  getRecommendations(artistName);
+}
+
+// Update form query selector and placeholder
+const form = document.querySelector('#artistForm');
+const artistInput = document.querySelector('#artist');
+artistInput.placeholder = 'Enter artist name';
 
 // Event listener for form submission
 form.addEventListener('submit', handleFormSubmit);
